a59b7c753f0fecb2748adc18dfc668205edd329a
KURAPATI SAI KIRAN <y22cse079@chalapathiengg.ac.in>
2026-02-28 07:01:42 +0000

    initial commit

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,103 +1,90 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller",
-  "sap/ui/model/Filter",
-  "sap/ui/model/FilterOperator",
-  "sap/ui/model/Sorter"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/Sorter"
 ], function (Controller, Filter, FilterOperator, Sorter) {
-  "use strict";
+    "use strict";
 
-  return Controller.extend("project2.controller.Main"), {
+    return Controller.extend("project2.controller.Main", { // Fixed: Parenthesis remains open here
 
-    onInit: function () {
-      this._aFilters = [];
-      this._sSearchQuery = "";
-      this._sCountry = "";
-      this._sSortKey = "desc";
+        onInit: function () {
+            this._sSearchQuery = "";
+            this._sCountry = "";
+            this._sSortKey = "desc";
+            
+            // Attach listener to update S.No after data is loaded
+            var oTable = this.byId("ordersTable");
+            oTable.getBinding("items").attachDataReceived(this._updateSNo, this);
+        },
 
-      this._applySorting();
-    },
+        onCountryChange: function (oEvent) {
+            this._sCountry = oEvent.getSource().getSelectedKey();
+            this._applyFilters();
+        },
 
-    /* ================= COUNTRY FILTER ================= */
-    onCountryChange: function (oEvent) {
-      this._sCountry = oEvent.getSource().getSelectedKey();
-      this._applyFilters();
-    },
+        onSortChange: function (oEvent) {
+            this._sSortKey = oEvent.getSource().getSelectedKey();
+            this._applySorting();
+        },
 
-    /* ================= SORT ================= */
-    onSortChange: function (oEvent) {
-      this._sSortKey = oEvent.getSource().getSelectedKey();
-      this._applySorting();
-    },
+        _applySorting: function () {
+            var oBinding = this.byId("ordersTable").getBinding("items");
+            if (oBinding) {
+                var bDescending = this._sSortKey === "desc";
+                oBinding.sort(new Sorter("OrderDate", bDescending));
+            }
+        },
 
-    _applySorting: function () {
-      var oTable = this.byId("ordersTable");
-      var oBinding = oTable.getBinding("items");
+        onSearchLiveChange: function (oEvent) {
+            this._sSearchQuery = oEvent.getParameter("newValue");
+            this._applyFilters();
+        },
 
-      if (!oBinding) return;
+        _applyFilters: function () {
+            var aFilters = [];
+            if (this._sCountry) {
+                aFilters.push(new Filter("ShipCountry", FilterOperator.EQ, this._sCountry));
+            }
 
-      var bDescending = this._sSortKey === "desc";
+            if (this._sSearchQuery) {
+                aFilters.push(new Filter({
+                    filters: [
+                        new Filter("OrderID", FilterOperator.Contains, this._sSearchQuery),
+                        new Filter("CustomerID", FilterOperator.Contains, this._sSearchQuery),
+                        new Filter("ShipCountry", FilterOperator.Contains, this._sSearchQuery)
+                    ],
+                    and: false
+                }));
+            }
 
-      var oSorter = new Sorter("OrderDate", bDescending);
-      oBinding.sort(oSorter);
-    },
+            var oBinding = this.byId("ordersTable").getBinding("items");
+            if (oBinding) {
+                oBinding.filter(aFilters);
+                this._updateSNo(); // Refresh S.No after filter
+            }
+        },
 
-    /* ================= SEARCH ================= */
-    onSearchLiveChange: function (oEvent) {
-      this._sSearchQuery = oEvent.getParameter("newValue");
-      this._applyFilters();
-    },
+        onOrderLinkPress: function (oEvent) {
+            this.getOwnerComponent().getRouter().navTo("orderDetails", {
+                orderId: oEvent.getSource().getText()
+            });
+        },
 
-    /* ================= APPLY FILTERS ================= */
-    _applyFilters: function () {
+        onCustomerLinkPress: function (oEvent) {
+            this.getOwnerComponent().getRouter().navTo("customerDashboard", {
+                customerId: oEvent.getSource().getText()
+            });
+        },
 
-      var aFilters = [];
-
-      // Country Filter
-      if (this._sCountry) {
-        aFilters.push(
-          new Filter("ShipCountry", FilterOperator.EQ, this._sCountry)
-        );
-      }
-
-      // Search Filter
-      if (this._sSearchQuery) {
-
-        var oSearchFilter = new Filter({
-          filters: [
-            new Filter("OrderID", FilterOperator.Contains, this._sSearchQuery),
-            new Filter("CustomerID", FilterOperator.Contains, this._sSearchQuery),
-            new Filter("ShipCountry", FilterOperator.Contains, this._sSearchQuery)
-          ],
-          and: false
-        });
-
-        aFilters.push(oSearchFilter);
-      }
-
-      var oTable = this.byId("ordersTable");
-      var oBinding = oTable.getBinding("items");
-
-      if (oBinding) {
-        oBinding.filter(aFilters);
-      }
-    },
-
-    /* ================= NAVIGATION ================= */
-
-    onOrderLinkPress: function (oEvent) {
-      var sOrderId = oEvent.getSource().getText();
-
-      this.getOwnerComponent().getRouter().navTo("order", {
-        orderId: sOrderId
-      });
-    },
-
-    onCustomerLinkPress: function (oEvent) {
-      var sCustomerId = oEvent.getSource().getText();
-
-      this.getOwnerComponent().getRouter().navTo("customerDashboard", {
-        customerId: sCustomerId
-      });
-    }
-
-  })
+        _updateSNo: function () {
+            var aItems = this.byId("ordersTable").getItems();
+            aItems.forEach(function (oItem, i) {
+                var oCells = oItem.getCells();
+                if (oCells[0]) {
+                    oCells[0].setText(i + 1);
+                }
+            });
+        }
+    }); // Fixed: Parenthesis correctly closes the extend function here
+});
